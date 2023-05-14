f25735a5ef999bd7e401906afdb5a8bdc2615844
Shakirova Merei <sh.merei05@gmail.com>
2023-05-14 21:49:47 +0600

    commit11

--- src/MyTestingClass.java
+++ src/MyTestingClass.java
@@ -1,3 +1,25 @@
 public class MyTestingClass {
+    private String name;
 
+    public MyTestingClass(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        for (int i = 0; i < name.length(); i++) {
+            hash = hash * 31 + name.charAt(i);
+        }
+
+    }
 }
+
